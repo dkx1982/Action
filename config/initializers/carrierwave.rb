ad33c4ff2b54e9aebb05f3993f880be2449bb4d5
“dkx1982” <dkx1982@163.com>
2017-02-20 14:33:52 +0800

    change gem fog to fog-aws-1

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,7 +11,7 @@ CarrierWave.configure do |config|
 
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
 
-      region:                'ap-northeast-1'
+      region:                'us-west-2'
 
     }
     config.fog_directory  = ENV["AWS_BUCKET_NAME"]

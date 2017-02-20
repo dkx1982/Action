a4f98a9949201f74cde66a6a6716922d22707659
“dkx1982” <dkx1982@163.com>
2017-02-20 14:43:26 +0800

    change gem fog to fog-aws-2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,14 +4,14 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.fog_provider = 'fog'
+    config.storage :fog
     config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
 
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
 
-      region:                'us-west-2'
+      region:                'ap-northeast-1'
 
     }
     config.fog_directory  = ENV["AWS_BUCKET_NAME"]
